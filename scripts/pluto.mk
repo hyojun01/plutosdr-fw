975e27f3557958ce5fd5e0112c4c1a6940e220b9
hyojun01 <joon_1234@kookmin.ac.kr>
2025-11-24 11:59:02 +0900

    ADD submodule linux and MODIFICATION for dts

--- scripts/pluto.mk
+++ scripts/pluto.mk
@@ -2,7 +2,8 @@
 # Target specific constants go here
 
 HDF_URL:=http://github.com/analogdevicesinc/plutosdr-fw/releases/download/${LATEST_TAG}/system_top.hdf
-TARGET_DTS_FILES:= zynq-pluto-sdr.dtb zynq-pluto-sdr-revb.dtb zynq-pluto-sdr-revc.dtb
+# hyojun: we use our Device Tree Blob.
+TARGET_DTS_FILES:= zynq-pluto-sdr-fmsdr.dtb zynq-pluto-sdr-revb-fmsdr.dtb zynq-pluto-sdr-revc-fmsdr.dtb
 COMPLETE_NAME:=PlutoSDR
 ZIP_ARCHIVE_PREFIX:=plutosdr
 DEVICE_VID:=0x0456
