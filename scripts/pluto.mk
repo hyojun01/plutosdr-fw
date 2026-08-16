f62ee8af3307ad5b4075169b9b6567af1211ced7
hyojun01 <joon_1234@kookmin.ac.kr>
2026-08-16 19:05:40 +0900

    pluto.its & pluto.mk: modify for new device-tree source

--- scripts/pluto.mk
+++ scripts/pluto.mk
@@ -2,9 +2,8 @@
 # Target specific constants go here
 
 HDF_URL:=http://github.com/analogdevicesinc/plutosdr-fw/releases/download/${LATEST_TAG}/system_top.hdf
-TARGET_DTS_FILES:= zynq-pluto-sdr.dtb zynq-pluto-sdr-revb.dtb zynq-pluto-sdr-revc.dtb
+TARGET_DTS_FILES:= zynq-pluto-rte.dtb zynq-pluto-rte-revb.dtb zynq-pluto-rte-revc.dtb
 COMPLETE_NAME:=PlutoSDR
 ZIP_ARCHIVE_PREFIX:=plutosdr
 DEVICE_VID:=0x0456
 DEVICE_PID:=0xb673
-
